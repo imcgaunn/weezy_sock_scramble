deb2a0dacf4aeb527feeb9adda7ecb8a0e9414ab
Ian McGaunn <ianmcgaunn@pressmail.ch>
2018-08-05 16:32:48 -0400

    Whoops, missed a few files in the last commit.

---
+++ sprites/weezy.py
@@ -0,0 +1,127 @@
+import logging
+
+import os
+import pygame
+
+from .block import Block
+from constants import SCREEN_X, SCREEN_Y
+
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("player")
+
+
+class Weezy(Block):
+    """ The player is a block that can walk """
+
+    X_VELOCITY = 6
+    Y_VELOCITY = 6
+
+    def __init__(self, color, width, height):
+        super().__init__(color, width, height)
+        self.playerheight = height
+        self.playerwidth = width
+        self.image_path = os.path.join('sprites', os.path.join('images', 'weez.gif'))
+        if pygame.image.get_extended():
+            self.image = pygame.image.load_basic(self.image_path)
+        else:
+            raise NameError("no GIF support :(")
+        self.change_x = 0
+        self.change_y = 0
+
+        self.keys_down = {pygame.K_DOWN: False,
+                          pygame.K_UP: False,
+                          pygame.K_RIGHT: False,
+                          pygame.K_LEFT: False}
+
+        self._score = 0
+
+    @property
+    def on_ground(self):
+        return self.rect.y >= SCREEN_Y - self.rect.height and self.change_y >= 0
+
+    @property
+    def can_jump(self):
+        # if we are on the ground or another platform, should be allowed to
+        # jump.
+        return True
+
+    @property
+    def score(self):
+        return self._score
+
+    def _apply_forces(self):
+        if self.change_y and not self.on_ground == 0:
+            self.change_y = 1  # start falling
+        else:
+            self.change_y += .6
+
+        if self.on_ground:
+            self.change_y = 0
+            self.rect.y = SCREEN_Y - self.rect.height
+
+    def update(self):
+        self._apply_forces()
+        self.rect.x += self.change_x
+        self.rect.y += self.change_y
+
+        # don't let player leave the screen; just stop moving
+        if self.rect.y > SCREEN_Y:
+            log.info(f"hit max y: {SCREEN_Y}")
+            self.rect.y = SCREEN_Y - self.playerheight  # you can't get out!
+        if self.rect.x > SCREEN_X:
+            log.info(f"hit max x: {SCREEN_X}")
+            self.rect.x = SCREEN_X - self.playerwidth
+            self.stop_movement()
+        if self.rect.y < 0:
+            log.info("hit min y: 0")
+            self.rect.y = 0
+        if self.rect.x < 0:
+            log.info("hit min x: 0")
+            self.rect.x = 0
+            self.stop_movement()
+
+    def handle_key_pressed(self, key):
+        try:
+            log.info(f'pressed: {key}')
+            if key == pygame.K_LEFT:
+                self.walk_left()
+            elif key == pygame.K_RIGHT:
+                self.walk_right()
+            elif key == pygame.K_DOWN:
+                pass
+            elif key == pygame.K_UP:
+                pass
+            elif key == pygame.K_SPACE:
+                self.jump()
+            self.keys_down[key] = True
+        except AttributeError:
+            log.info("you didn't pass a keyboard event!!")
+
+    def handle_key_released(self, key):
+        def _opposite_dir(key):
+            return {pygame.K_LEFT: pygame.K_RIGHT,
+                    pygame.K_RIGHT: pygame.K_LEFT,
+                    pygame.K_UP: pygame.K_DOWN}[key]
+        try:
+            log.info(f'released: {key}')
+            self.keys_down[key] = False
+            if key in {pygame.K_LEFT, pygame.K_RIGHT} and not(self.keys_down[_opposite_dir(key)]):
+                self.stop_movement()
+            print(f'keys down: {self.keys_down}')
+        except AttributeError:
+            log.info("you didn't pass a keyboard event!!")
+
+    def walk_left(self):
+        self.change_x = -Weezy.X_VELOCITY
+
+    def walk_right(self):
+        self.change_x = Weezy.X_VELOCITY
+
+    def stop_movement(self):
+        self.change_x = 0
+
+    def jump(self):
+        if self.can_jump:
+            self.change_y = -10
+
---
+++ sprites/world.py
@@ -0,0 +1,59 @@
+import pygame
+import logging
+
+from constants import WHITE
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("world")
+
+
+class World(object):
+    """ Holds all the sprite groups """
+    def __init__(self, screen):
+        self._all = pygame.sprite.Group()
+        self._blocks = pygame.sprite.Group()
+        self._player = pygame.sprite.GroupSingle()
+        self._screen = screen
+        self._score = 0
+        self._clock = pygame.time.Clock()
+
+    def add_block(self, block):
+        self._blocks.add(block)
+        self._all.add(block)
+
+    def set_player(self, player):
+        self._player.add(player)
+        self._all.add(player)
+
+    def handle_key_pressed(self, kb_event):
+        try:
+            log.info(f'pressed: {kb_event}')
+            self._player.sprite.handle_key_pressed(kb_event.key)
+        except AttributeError:
+            log.info("you didn't pass a keyboard event!!")
+        finally:
+            return
+
+    def handle_key_released(self, kb_event):
+        try:
+            log.info(f"released: {kb_event}")
+            if kb_event.key in {pygame.K_LEFT, pygame.K_RIGHT, pygame.K_UP, pygame.K_DOWN}:
+                self._player.sprite.handle_key_released(kb_event.key)
+        except AttributeError:
+            log.info("you didn't pass a keyboard event!!")
+        finally:
+            return
+
+    def turn(self):
+        self._screen.fill(WHITE)
+
+        blocks_hit_list = pygame.sprite.spritecollide(self._player.sprite,
+                                                      self._blocks, False)
+        for block in blocks_hit_list:
+            self._score += 1
+            block.reset_pos()
+
+        self._all.update()
+        self._all.draw(self._screen)
+        self._clock.tick(60)
+        pygame.display.flip()
