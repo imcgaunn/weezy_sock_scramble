9a3a7ea6e41260ce559de2f59429cacc043ebac1
Ian McGaunn <ianmcgaunn@pressmail.ch>
2018-08-05 22:11:52 -0400

    Remove extra cruft

--- sprites/basic_platform.py
+++ sprites/basic_platform.py
@@ -1,9 +1,6 @@
 import logging
 
-import os
 import pygame
-
-from constants import SCREEN_X, SCREEN_Y, FACING_RIGHT, FACING_LEFT
 
 
 logging.basicConfig(level=logging.INFO)
@@ -11,5 +8,4 @@ log = logging.getLogger("basic_platform")
 
 
 class BasicPlatform(pygame.sprite.Sprite):
-
     pass
